760f1583567e005a2bf4d5b734ea738853f8360c
Jonathan Cancio <jonahcancio@gmail.com>
2018-09-08 22:35:34 +0800

    Outputs data on html and overwrites as well

--- CrockPot.java
+++ CrockPot.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 import java.io.*;
+import java.lang.StringBuilder;
 
 public class CrockPot {
+	static String outputFileName = "JandelOutput.html";
 	static String taskListDemo = "adobo 1\n"
 		+ "tinola 5\n"
 		+ "karekare 8\n"
@@ -15,17 +17,26 @@ public class CrockPot {
 		+ "cook 10\n";
 	static ArrayList<Dish> gordonQueue = new ArrayList<Dish>();
 	static HashMap<String, Integer> gordonMap = new HashMap<String, Integer>();
+	static String gordonTable = "";
+	static String gordonHtml = "";
 
 	public static void main(String args[]) throws IOException{
 		FileReader taskFile = new FileReader("recipes/tasklist.txt");
 		BufferedReader taskReader = new BufferedReader(taskFile);
 
-		// stringDemoLoop();
+
 		fileDemoLoop(taskReader);
+		initGordonHtml();
 
+		initGordonTable();
+		insertGordonTable();
+		System.out.println(gordonHtml);
+		outputGordonHtml();
 
-
+		taskReader.close();
+		taskFile.close();
 	}
+
 
 	static void fileDemoLoop(BufferedReader taskReader) throws IOException{
 		String taskLine;
@@ -49,7 +60,6 @@ public class CrockPot {
 			gordonQueue.add(newDish);
 		}
 
-		consoleLogGordonQueue();
 	}
 
 	static void stringDemoLoop() {
@@ -73,7 +83,7 @@ public class CrockPot {
 
 	static void consoleLogGordonQueue() {
 		for (Dish dish : gordonQueue){
-			System.out.println(dish.name + ":");
+			System.out.println(dish.name + " - " + dish.startTime +":");
 			for(Action act : dish.aQueue) {
 				System.out.println("\t=> " + act.name + " - " + act.timeLeft);
 			}
@@ -81,4 +91,70 @@ public class CrockPot {
 		}
 	}
 
+
+	static void initGordonTable() {
+		gordonTable += "<table class='ui celled padded table'>" + System.lineSeparator();
+		gordonTable += "\t<thead>" + System.lineSeparator()
+			+ "\t\t<tr>" +  System.lineSeparator()
+			+ "\t\t\t<th>Time</th>" +  System.lineSeparator()
+			+ "\t\t\t<th>Dish</th>" +  System.lineSeparator()
+			+ "\t\t\t<th>Actions</th>" +  System.lineSeparator()
+			+ "\t\t\t<th>TimeLefts</th>" +  System.lineSeparator()
+			+ "\t\t\t<th>Remarks</th>" +  System.lineSeparator()
+			+ "\t\t</tr>" +  System.lineSeparator()
+			+ "\t</thead>" +  System.lineSeparator();
+
+		gordonTable += "\t<tbody>" + System.lineSeparator();
+		int i = 1;
+		for (Dish dish : gordonQueue){
+			gordonTable += "\t\t<tr>" +  System.lineSeparator();
+			gordonTable += "\t\t\t<td>" + i + "</td>" + System.lineSeparator();
+			gordonTable += "\t\t\t<td>" + dish.name + " - " + dish.startTime + "</td>" + System.lineSeparator();
+			gordonTable += "\t\t\t<td>" + System.lineSeparator();
+			for(Action act : dish.aQueue) {
+				gordonTable += "\t\t\t\t<p>" + act.name + " - " + act.timeLeft + "</p" + System.lineSeparator();
+			}
+			gordonTable += "\t\t\t</td>" + System.lineSeparator();
+			i++;
+			gordonTable += "\t\t</tr>" +  System.lineSeparator();
+		}
+		gordonTable += "\t</tbody>" + System.lineSeparator();
+		gordonTable += "</table>" + System.lineSeparator();
+	}
+
+	static void initGordonHtml() throws IOException {
+		FileReader outputFile = new FileReader(outputFileName);
+		BufferedReader outputReader = new BufferedReader(outputFile);
+		String htmlLine;
+		while ( (htmlLine = outputReader.readLine() ) != null) {
+			gordonHtml += htmlLine + System.lineSeparator();
+		}
+		outputReader.close();
+		outputFile.close();
+	}
+
+	static void outputGordonHtml() throws IOException{
+		FileWriter outputFile = new FileWriter(outputFileName);
+		outputFile.write(gordonHtml);
+		outputFile.close();
+	}
+
+	static void insertGordonTable() throws IOException{
+		clearHtmlTable();
+
+		String startTag = "<!--START HERE-->" + System.lineSeparator();
+		StringBuilder gordonBuilder = new StringBuilder(gordonHtml);
+		gordonBuilder.insert(gordonHtml.indexOf(startTag) + startTag.length(), gordonTable);
+		gordonHtml = gordonBuilder.toString();
+	}
+
+	static void clearHtmlTable() {
+		String startTag = "<!--START HERE-->" + System.lineSeparator();
+		String endTag = "<!--END HERE-->";
+		StringBuilder gordonBuilder = new StringBuilder(gordonHtml);
+		int startIndex = gordonBuilder.indexOf(startTag) + startTag.length();
+		int endIndex = gordonBuilder.indexOf(endTag);
+		gordonBuilder.delete(startIndex, endIndex);
+		gordonHtml = gordonBuilder.toString();
+	}
 }
