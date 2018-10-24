dc89f0e42c6f28b18e6fc4c860911ff515c65dcc
Mikhael Dionio <mikhaeldionio@gmail.com>
2018-10-24 22:09:03 +0800

    properly integrate PRIORITY into IronChef

--- PRIORITY.java
+++ PRIORITY.java
@@ -3,59 +3,117 @@ import java.util.*;
 public class PRIORITY extends Scheduler{
 
   ArrayList<Dish> alphabetSortDishes(ArrayList<Dish> dishList){//sort dishes in list alphabetically
-    ArrayList<Dish> sortedDishList = new ArrayList<Dish>();
+    //ArrayList<Dish> sortedDishList = new ArrayList<Dish>();
+    if(!dishList.isEmpty()){
+      // while(!dishList.isEmpty()){//Iterate until list is empty (meaning all elements are in the output list)
+      //   //Finding the "smallest" dish
+      //   int key;
+      //   Dish ref;
+      //   key = 0;
+      //   ref = dishList.get(0);
+      //
+      //   for(int i = 0; i<dishList.size(); i++){
+      //     Dish dish = dishList.get(i);
+      //     if(ref.name.compareTo(dish.name) > 0){//If we find a "smaller" dish
+      //       key = i;
+      //       ref = dish;
+      //     }
+      //   }
+      //   dishList.remove(key);//remove "smallest" dish from old list
+      //   sortedDishList.add(ref);//add "smallest" dish to new list
+      // }
 
-    while(dishList.get(0) != null){//Iterate until list is empty (meaning all elements are in the output list)
-      //Finding the "smallest" dish
-      int key;
-      Dish ref;
-      key = 0;
-      ref = dishList.get(0);
-
-      for(int i = 0; i<dishList.size(); i++){
-        Dish dish = dishList.get(i);
-
-        if(ref.name.compareTo(dish.name) > 0){//If we find a "smaller" string
-          key = i;
-          ref = dish;
-        }
-
-        }
-      }
-
-      dishList.remove(key);//remove "smallest" dish from old list
-      sortedDishList.add(ref);//add "smallest" dish to new list
+      Collections.sort(dishList, new SortbyName());
+    }else{
+      System.out.println("hello");
+      return dishList;
     }
-    return sortedDishList;
+    return dishList;
   }
 
-  public Dish whatIsCookNext(Dish dishBeingCooked, ArrayList<Dish> dishList){
-      Dish nextDish;
-      //sort dishList alphabetically
-      dishList = alphabetSortDishes(dishList);
-
-      //get 0
-      nextDish = dishList.get(0);
-      return nextDish;
+  private class SortbyName implements Comparator<Dish>{
+    public int compare(Dish a, Dish b){
+      String aName = a.name;
+      String bName = b.name;
+      return aName.compareTo(b.name);
+    }
   }
 
-  public String cookUpdate(){//updates ReadyQueue and current dish at every time increment
-      String cookString = "";
-      if(dishBeingCooked != null && dishBeingCooked.aQueue.get(dishBeingCooked.currentActionIndex).timeLeft == 1){//if dish is done cooking
-          cookString = dishBeingCooked.name + " cook done, ";
-          dishBeingCooked.currentActionIndex++;
-          if(dishBeingCooked.currentActionIndex < dishBeingCooked.aQueue.size()){
-              AssistantQueue.add(dishBeingCooked);
-          }
-          dishBeingCooked = null;
-      }
+    public Dish whatIsCookNext(Dish dishBeingCooked){
+        Dish nextDish;
+        nextDish = ReadyQueue.get(0);
+        ReadyQueue.remove(0);
+        return nextDish;
+    }
 
-      if(dishBeingCooked == null && !ReadyQueue.isEmpty()){//if there is no dish being cooked and there are dishes to be cooked, cook next dish
-          dishBeingCooked = whatIsCookNext(dishBeingCooked, ReadyQueue);
-      }else if(dishBeingCooked != null){//if there is a dish cooking, subtract one from its time
-          dishBeingCooked.aQueue.get(dishBeingCooked.currentActionIndex).timeLeft--;
-      }
+    public String readyUpdate(){//adds additional dishes to the ReadyQueue based on the tasklist
+        String readyString = "";
+        int i = 0;
+        while(i < crockpot.gordonQueue.size()){
+            Dish dish = crockpot.gordonQueue.get(i);
+            if(time == dish.startTime){
+                readyString = readyString + dish.name + " arrives, ";
+                ReadyQueue.add(dish);
+                ReadyQueue = alphabetSortDishes(ReadyQueue);
+                crockpot.gordonQueue.remove(i);
+            }else{
+                i++;
+            }
+        }
 
-      return cookString;
-  }
+        return readyString;
+    }
+
+    public String assistantUpdate(){//updates the assistant queue at everytime increment (do this before the cookingUpdate)
+        String assistantString = "";
+        int i = 0;
+        while(i < ReadyQueue.size()){//look for any dishes that require assistance
+            if(!ReadyQueue.get(i).aQueue.get(ReadyQueue.get(i).currentActionIndex).name.equals("cook")){
+
+                AssistantQueue.add(ReadyQueue.get(i));
+                ReadyQueue.remove(i);
+            }else{
+                i++;
+            }
+        }
+
+        i = 0;
+        while(i < AssistantQueue.size()){//check for any dish done with current assistance task
+            Dish dish = AssistantQueue.get(i);
+            //System.out.println(dish.currentActionIndex);
+            if(dish.aQueue.get(dish.currentActionIndex).timeLeft == 1){
+                assistantString = assistantString + dish.name + " " + dish.aQueue.get(dish.currentActionIndex).name + " done, ";
+                dish.currentActionIndex++;
+                //System.out.println(dish.currentActionIndex);
+                if(dish.currentActionIndex >= dish.aQueue.size()){//if no more actions left, remove from queue forever
+                    AssistantQueue.remove(i);
+                    assistantString = assistantString + dish.name + " " + " dish done, ";
+                }else{
+                    ReadyQueue.add(dish);
+                    ReadyQueue = alphabetSortDishes(ReadyQueue);
+                    AssistantQueue.remove(i);
+                }
+            }else{
+            i++;
+            }
+            //System.out.print(i);
+        }
+
+        for(Dish dishAssisted : AssistantQueue){//subtracts one from all assistant actions
+            Action a;
+            a = dishAssisted.aQueue.get(dishAssisted.currentActionIndex);
+            if(dishAssisted.aQueue.get(dishAssisted.currentActionIndex).timeLeft > 0){
+                a.timeLeft--;
+            }
+        }
+
+        for(Dish dish : AssistantQueue){//check for any dish done with current assistance task, move on to the next action, this has to be done after subtracting the currently on going assistance tasks
+            if(dish.aQueue.get(dish.currentActionIndex).timeLeft == 0){
+                dish.currentActionIndex++;
+                //System.out.println(dish.currentActionIndex);
+            }
+        }
+
+        return assistantString;
+    }
 }
